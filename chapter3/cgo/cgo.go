e5ded15cff505bc9a0f7b1c0f58a111be218bdbd
Andy Williams <andy@andywilliams.me>
2018-07-28 15:58:38 +0100

    CString should be freed

--- chapter3/cgo/cgo.go
+++ chapter3/cgo/cgo.go
@@ -2,14 +2,17 @@ package main
 
 /*
 #include <stdio.h>
+#include <stdlib.h>
 
 void print_hello(const char *name) {
 	printf("Hello %s!\n", name);
 }
 */
 import "C"
+import "unsafe"
 
 func main() {
 	cName := C.CString("World")
 	C.print_hello(cName)
+	C.free(unsafe.Pointer(cName))
 }
