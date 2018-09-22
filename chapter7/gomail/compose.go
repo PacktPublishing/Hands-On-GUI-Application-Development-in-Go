2d23909d3939b29b347f550fe98e1769a89a6ded
Andy Williams <andy@andywilliams.me>
2018-09-22 22:34:00 +0100

    Update GoMail for completed chapter 7 (Qt)

--- chapter7/gomail/compose.go
+++ chapter7/gomail/compose.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"github.com/PacktPublishing/Hands-On-GUI-Application-Development-in-Go/client"
 	"github.com/therecipe/qt/widgets"
+	"time"
 )
 
-func showCompose() {
+type composeUI struct {
+	server *client.EmailServer
+
+	dialog      *widgets.QDialog
+	to, subject *widgets.QLineEdit
+	content     *widgets.QTextEdit
+}
+
+func (c *composeUI) createEmail() *client.EmailMessage {
+	email := &client.EmailMessage{}
+
+	email.Subject = c.subject.Text()
+	email.To = client.Email(c.to.Text())
+	email.Date = time.Now()
+	email.Content = c.content.ToPlainText()
+
+	return email
+}
+
+func (c *composeUI) show() {
 	dialog := widgets.NewQDialog(nil, 0)
 	dialog.SetModal(false)
 	dialog.SetWindowTitle("New GoMail")
+	c.dialog = dialog
 
 	form := widgets.NewQFormLayout(dialog)
 	dialog.SetLayout(form)
@@ -20,11 +42,24 @@ func showCompose() {
 	buttons := widgets.NewQWidget(dialog, 0)
 	buttons.SetLayout(widgets.NewQHBoxLayout())
 	buttons.Layout().AddItem(widgets.NewQSpacerItem(0, 0, widgets.QSizePolicy__Expanding, 0))
-	buttons.Layout().AddWidget(widgets.NewQPushButton2("Cancel", buttons))
+	cancel := widgets.NewQPushButton2("Cancel", buttons)
+	cancel.ConnectClicked(func(_ bool) {
+		c.dialog.Close()
+	})
+	buttons.Layout().AddWidget(cancel)
 	send := widgets.NewQPushButton2("Send", buttons)
 	send.SetDefault(true)
+	send.ConnectClicked(func(_ bool) {
+		email := c.createEmail()
+		c.server.Send(email)
+		c.dialog.Close()
+	})
 	buttons.Layout().AddWidget(send)
 	form.AddRow5(buttons)
 
 	dialog.Show()
+}
+
+func newCompose(server *client.EmailServer) *composeUI {
+	return &composeUI{server: server}
 }
--- chapter7/gomail/main.go
+++ chapter7/gomail/main.go
@@ -1,58 +1,35 @@
 package main
 
 import (
+	"github.com/PacktPublishing/Hands-On-GUI-Application-Development-in-Go/client"
+
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/gui"
 	"github.com/therecipe/qt/widgets"
 	"os"
 )
 
-/*
-func (g *GoMailUIBrowse) buildEmailActions() []MenuItem {
-	return []MenuItem{
-		Action{
-			Text: "New",
-			OnTriggered: func() {
-				new(GoMailUICompose).Show(g.window)
-			},
-		},
-		Action{
-			Text: "Reply",
-		},
-		Action{
-			Text: "Reply All",
-		},
-		Separator{},
-		Action{
-			Text: "Delete",
-		},
-	}
+type mainUI struct {
+	core.QObject
+	_     func(message *client.EmailMessage) `signal:"newMail"`
+	model *core.QStringListModel
+
+	app    *widgets.QApplication
+	server *client.EmailServer
+
+	subject, to, from, date, content *widgets.QLabel
 }
 
-func (g *GoMailUIBrowse) buildEditActions() []MenuItem {
-	return []MenuItem{
-		Action{
-			Text: "Cut",
-		},
-		Action{
-			Text: "Copy",
-		},
-		Action{
-			Text: "Paste",
-		},
-	}
-}
-*/
-func buildMenu(app *widgets.QApplication) *widgets.QMenuBar {
+func (m *mainUI) buildMenu() *widgets.QMenuBar {
 	menu := widgets.NewQMenuBar(nil)
 
 	file := widgets.NewQMenu2("File", menu)
 	new := file.AddAction("New")
-	new.ConnectTriggered(func(_ bool){showCompose()})
+	new.ConnectTriggered(func(_ bool) { newCompose(m.server).show() })
 	file.AddSeparator()
 	quit := file.AddAction("Quit")
-	quit.ConnectTriggered(func (_ bool) {
-		app.QuitDefault()
+	quit.ConnectTriggered(func(_ bool) {
+		m.app.QuitDefault()
 	})
 	menu.AddMenu(file)
 
@@ -62,12 +39,12 @@ func buildMenu(app *widgets.QApplication) *widgets.QMenuBar {
 	return menu
 }
 
-func buildToolbar() *widgets.QToolBar {
+func (m *mainUI) buildToolbar() *widgets.QToolBar {
 	toolbar := widgets.NewQToolBar("tools", nil)
 	toolbar.SetToolButtonStyle(core.Qt__ToolButtonTextUnderIcon)
 
-	new := toolbar.AddAction2(gui.QIcon_FromTheme2("document-new", nil), "New")
-	new.ConnectTriggered(func(_ bool){showCompose()})
+	docNew := toolbar.AddAction2(gui.QIcon_FromTheme2("document-new", nil), "New")
+	docNew.ConnectTriggered(func(_ bool) { newCompose(m.server).show() })
 	toolbar.AddAction("Reply")
 	toolbar.AddAction("Reply All")
 	toolbar.AddSeparator()
@@ -81,10 +58,7 @@ func buildToolbar() *widgets.QToolBar {
 	return toolbar
 }
 
-
-func main() {
-	app := widgets.NewQApplication(len(os.Args), os.Args)
-
+func (m *mainUI) buildUI() *widgets.QMainWindow {
 	window := widgets.NewQMainWindow(nil, 0)
 	window.SetWindowTitle("GoMail")
 
@@ -93,28 +67,77 @@ func main() {
 	window.SetMinimumSize2(600, 400)
 	window.SetCentralWidget(widget)
 
-	window.SetMenuBar(buildMenu(app))
-	widget.Layout().AddWidget(buildToolbar())
+	window.SetMenuBar(m.buildMenu())
+	widget.Layout().AddWidget(m.buildToolbar())
 
-	model := core.NewQStringListModel2([]string{"email1", "email2"}, widget)
-	model.SetHeaderData(0, core.Qt__Horizontal, core.NewQVariant14("Inbox"), 0)
+	subjects := []string{}
+	for _, message := range m.server.ListMessages() {
+		subjects = append(subjects, message.Subject)
+	}
+	m.model = core.NewQStringListModel2(subjects, widget)
+	m.model.SetHeaderData(0, core.Qt__Horizontal, core.NewQVariant14("Inbox"), 0)
 	list := widgets.NewQTreeView(window)
-	list.SetModel(model)
+	list.SetModel(m.model)
+	list.ConnectSelectionChanged(func(selected *core.QItemSelection, _ *core.QItemSelection) {
+		if len(selected.Indexes()) == 0 {
+			return
+		}
+
+		row := selected.Indexes()[0].Row()
+		m.setMessage(m.server.ListMessages()[row])
+	})
 
 	detail := widgets.NewQWidget(window, 0)
 	form := widgets.NewQFormLayout(detail)
 	detail.SetLayout(form)
-	form.AddRow5(widgets.NewQLabel2("subject", detail, 0))
-	form.AddRow3("From", widgets.NewQLabel2("email", detail, 0))
-	form.AddRow3("To", widgets.NewQLabel2("email", detail, 0))
-	form.AddRow3("Date", widgets.NewQLabel2("date", detail, 0))
-	form.AddRow5(widgets.NewQLabel2("content", detail, 0))
+
+	m.subject = widgets.NewQLabel2("subject", detail, 0)
+	form.AddRow5(m.subject)
+	m.from = widgets.NewQLabel2("email", detail, 0)
+	form.AddRow3("From", m.from)
+	m.to = widgets.NewQLabel2("email", detail, 0)
+	form.AddRow3("To", m.to)
+	m.date = widgets.NewQLabel2("date", detail, 0)
+	form.AddRow3("Date", m.date)
+	m.content = widgets.NewQLabel2("content", detail, 0)
+	form.AddRow5(m.content)
 
 	splitter := widgets.NewQSplitter(window)
 	splitter.AddWidget(list)
 	splitter.AddWidget(detail)
 	widget.Layout().AddWidget(splitter)
 
+	return window
+}
+
+func (m *mainUI) setMessage(message *client.EmailMessage) {
+	m.subject.SetText(message.Subject)
+	m.to.SetText(message.ToEmailString())
+	m.from.SetText(message.FromEmailString())
+	m.date.SetText(message.DateString())
+
+	m.content.SetText(message.Content)
+}
+
+func (m *mainUI) prependEmail(message *client.EmailMessage) {
+	m.model.SetStringList(append([]string{message.Subject}, m.model.StringList()...))
+}
+
+func main() {
+	main := NewMainUI(nil)
+	main.app = widgets.NewQApplication(len(os.Args), os.Args)
+	main.server = client.NewTestServer()
+
+	window := main.buildUI()
+	main.setMessage(main.server.CurrentMessage())
 	window.Show()
+
+	main.ConnectNewMail(main.prependEmail)
+	go func() {
+		for email := range main.server.Incoming() {
+			main.NewMail(email)
+		}
+	}()
+
 	widgets.QApplication_Exec()
 }
