e67898a9a8f44263d0ce295f5d468701e7458f98
Andy Williams <andy@andy.xyz>
2018-12-31 12:50:46 +0000

    Fix formatting with gofmt

--- chapter13/platform/notification_darwin.go
+++ chapter13/platform/notification_darwin.go
@@ -6,8 +6,8 @@ import (
 )
 
 func notify(title, content string) {
-	cmd := exec.Command("osascript", "-e", "display notification \"" + content +
-		"\" with title \"" + title + "\"")
+	cmd := exec.Command("osascript", "-e", "display notification \""+content+
+		"\" with title \""+title+"\"")
 	err := cmd.Run()
 
 	if err != nil {
